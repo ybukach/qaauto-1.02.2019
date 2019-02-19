aba741e12913d12b731162b88c5e97ce19b35dea
Yulia Bukach <juliabukach@gmail.com>
2019-02-19 17:20:59 +0200

    Lesson 5

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -9,7 +9,7 @@ import org.testng.annotations.Test;
 public class LoginTests {
 
     @Test
-    public void negativeLoginTest() {
+    public void negativeLoginTestWrongEmailEmptyPassword() {
 
         System.setProperty("webdriver.chrome.driver", "/Users/yulia.bukach/Downloads/chromedriver");
 
@@ -40,8 +40,90 @@ public class LoginTests {
         landingPage.Login("nct.test1@gmail.com", "Bukach2019");
 
         HomePage homePage = new HomePage(driver);
-        Assert.assertTrue(homePage.isPageLoaded(), "Home page is not loaded.") ;
+        Assert.assertTrue(homePage.isPageLoaded(), "Home page is not loaded.");
 
     }
+
+    @Test
+    public void negativeLoginTestEmptyEmailWrongPassword() {
+        System.setProperty("webdriver.chrome.driver", "/Users/yulia.bukach/Downloads/chromedriver");
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.linkedin.com/");
+
+        LandingPage landingPage = new LandingPage(driver);
+        Assert.assertTrue(landingPage.isPageLoaded(), "Landing page is not loaded.");
+
+        landingPage.Login("", "123");
+
+        Assert.assertTrue(landingPage.isPageLoaded(), "landing page is not loaded");
+
+    }
+
+    @Test
+    public void negativeLoginTestWrongEmailCorrectPassword() {
+        System.setProperty("webdriver.chrome.driver", "/Users/yulia.bukach/Downloads/chromedriver");
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.linkedin.com/");
+        LandingPage landingPage = new LandingPage(driver);
+        Assert.assertTrue(landingPage.isPageLoaded(), "Landing page is not loaded.");
+
+        landingPage.Login("a@b.c", "Bukach2019");
+
+        LoginToLinkedinPage loginToLinkedinPage = new LoginToLinkedinPage(driver);
+
+        Assert.assertTrue(loginToLinkedinPage.isLoginToLinkedinPageLoaded(), "Credentials validation is wrong");
+    }
+
+    @Test
+    public void negativeLoginTestCorrectEmailWrongPassword() {
+        System.setProperty("webdriver.chrome.driver", "/Users/yulia.bukach/Downloads/chromedriver");
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.linkedin.com/");
+        LandingPage landingPage = new LandingPage(driver);
+        Assert.assertTrue(landingPage.isPageLoaded(), "Landing page is not loaded.");
+
+        landingPage.Login("nct.test1@gmail.com", "111");
+
+        LoginToLinkedinPage loginToLinkedinPage = new LoginToLinkedinPage(driver);
+
+        Assert.assertTrue(loginToLinkedinPage.isLoginToLinkedinPageLoaded(), "Credentials validation is wrong");
+    }
+
+    @Test
+    public void negativeLoginTestCleansingCorrectEmailCleansingWrongPassword() {
+        System.setProperty("webdriver.chrome.driver", "/Users/yulia.bukach/Downloads/chromedriver");
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.linkedin.com/");
+        LandingPage landingPage = new LandingPage(driver);
+        Assert.assertTrue(landingPage.isPageLoaded(), "Landing page is not loaded.");
+
+        landingPage.Login("      nct.test1@gmail.com     ", "     Bukach2019111     ");
+
+        LoginToLinkedinPage loginToLinkedinPage = new LoginToLinkedinPage(driver);
+
+        Assert.assertTrue(loginToLinkedinPage.isLoginToLinkedinPageLoaded(), "Credentials validation is wrong");
+
+    }
+
+    @Test
+    public void negativeLoginTestDifferentCaseInCorrectEmailDifferentCaseInCorrectPassword() {
+        System.setProperty("webdriver.chrome.driver", "/Users/yulia.bukach/Downloads/chromedriver");
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.linkedin.com/");
+        LandingPage landingPage = new LandingPage(driver);
+        Assert.assertTrue(landingPage.isPageLoaded(), "Landing page is not loaded.");
+
+        landingPage.Login("Nct.test1@Gmail.Com", "BukacH2019");
+
+        LoginToLinkedinPage loginToLinkedinPage = new LoginToLinkedinPage(driver);
+
+        Assert.assertTrue(loginToLinkedinPage.isLoginToLinkedinPageLoaded(), "Credentials validation is wrong");
+    }
+
 
 }
